c650e08b425ddf8d1ae5dc4ee22be08dbb694000
VKnyshov <valeriy.knyshov@gmail.com>
2024-12-16 15:06:45 +0200

    all in

---
+++ apps/pizza/filter.py
@@ -0,0 +1,20 @@
+from django.http import QueryDict
+from rest_framework.exceptions import ValidationError
+from django.db.models import QuerySet
+
+from apps.pizza.models import PizzaModel
+
+
+def filter_pizza(query:QueryDict)->QuerySet:
+    qs = PizzaModel.objects.all()
+
+    for k,v in query.items():
+        match k:
+            case 'price_gt':
+                qs = qs.filter(price__gt=v)
+            case 'price_lt':
+                qs = qs.filter(price__lt=v)
+            case _:
+                raise ValidationError({'detail':f'"{k}" not allowed'})
+
+    return qs
