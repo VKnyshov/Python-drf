20f57157f5ba2f95a874d99d84fc0ed26118297b
VKnyshov <valeriy.knyshov@gmail.com>
2024-12-24 23:46:33 +0200

    all in

---
+++ apps/user/views.py
@@ -0,0 +1,9 @@
+from rest_framework.generics import ListCreateAPIView
+from django.contrib.auth import get_user_model
+
+from apps.user.serializers import UserSerializer
+
+UserModel = get_user_model()
+class UserListCreateView(ListCreateAPIView):
+    queryset = UserModel.objects.all()
+    serializer_class = UserSerializer
