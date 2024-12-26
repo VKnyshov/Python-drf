ad4bb56c3bb0941e120303acfe5717a7b5274757
VKnyshov <valeriy.knyshov@gmail.com>
2024-12-26 22:33:31 +0200

    all in

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import UserListCreateView
+from .views import UserListCreateView, UserToAdminView, BlockUserView, UnBlockUserView
 urlpatterns = [
 
     path('', UserListCreateView.as_view(), name='user_list_create'),
+    path('/<int:pk>/block', BlockUserView.as_view(), name='user_block_user'),
+    path('/<int:pk>/unblock', UnBlockUserView.as_view(), name='user_unblock_user'),
+    path('/<int:pk>/to_admin', UserToAdminView.as_view(), name='user_to_admin'),
 ]
--- apps/user/views.py
+++ apps/user/views.py
@@ -1,5 +1,7 @@
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, GenericAPIView
 from django.contrib.auth import get_user_model
+from rest_framework.response import Response
+from rest_framework import status
 
 from apps.user.serializers import UserSerializer
 
@@ -7,3 +9,49 @@ UserModel = get_user_model()
 class UserListCreateView(ListCreateAPIView):
     queryset = UserModel.objects.all()
     serializer_class = UserSerializer
+
+###########################################################################
+
+class BlockUserView(GenericAPIView):
+    # queryset = UserModel.objects.all()
+    def get_queryset(self):
+        return UserModel.objects.all().exclude(id=self.request.user.id) #можна заблокувати всіх окрім залогіненого
+
+    def patch(self, *args, **kwargs):
+        user = self.get_object()
+        if user.is_active:
+            user.is_active = False
+            user.save()
+
+        serializer = UserSerializer(user)
+        return Response(serializer.data, status.HTTP_200_OK)
+
+
+class UnBlockUserView(GenericAPIView):
+    # queryset = UserModel.objects.all()
+    def get_queryset(self):
+        return UserModel.objects.exclude(id=self.request.user.id) #можна заблокувати всіх окрім залогіненого
+
+    def patch(self, *args, **kwargs):
+        user = self.get_object()
+        if not user.is_active:
+            user.is_active = True
+            user.save()
+
+        serializer = UserSerializer(user)
+        return Response(serializer.data, status.HTTP_200_OK)
+
+
+class UserToAdminView(GenericAPIView):
+    # queryset = UserModel.objects.all()
+    def get_queryset(self):
+        return UserModel.objects.exclude(id=self.request.user.id) #можна заблокувати всіх окрім залогіненого
+
+    def patch(self, *args, **kwargs):
+        user = self.get_object()
+        if not user.is_staff:
+            user.is_staff = True
+            user.save()
+
+        serializer = UserSerializer(user)
+        return Response(serializer.data, status.HTTP_200_OK)
