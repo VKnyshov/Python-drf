00467ac6e3615b1e04d3e9fc694758c24c302af5
VKnyshov <valeriy.knyshov@gmail.com>
2025-01-14 10:20:23 +0200

    les9-all good

--- backend/apps/chat/models.py
+++ backend/apps/chat/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+UserModel = get_user_model()
+class ChatRoomModel(models.Model):
+    class Meta:
+        db_table = 'chat_room'
+    name = models.CharField(max_length=50)
 
-# Create your models here.
+    def __str__(self):
+        return self.name
+
+class MessageModel(models.Model):
+    class Meta:
+        db_table = 'chat_message'
+
+    room = models.ForeignKey(ChatRoomModel, on_delete=models.CASCADE, related_name='messages')
+    user = models.ForeignKey(UserModel, on_delete=models.CASCADE, related_name='messages')
+    text = models.TextField()
--- backend/configs/settings.py
+++ backend/configs/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'apps.user',
     'apps.pizza',
     'apps.pizza_shop',
+    'apps.chat',
 ]
 
 MIDDLEWARE = [
